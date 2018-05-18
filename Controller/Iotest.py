01d5ca414f907e420c4f54dc851a641fb2c7e264
wangmm1994 <769669064@qq.com>
2018-05-18 17:03:34 +0800

    delete

--- Controller/Iotest.py
+++
@@ -1,7 +0,0 @@
-srt = 'https://images.unsplash.com/photo-1491974162517-a7541284efe7?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e0c4664b504f632d31f49fb757f9335f&w=1000&q=80'
-width_pos = srt.index('&w=')
-height_pos = srt.index('&q=')
-width_height_str = srt[width_pos :] #使用切片功能截取高度和宽度参数，后面用来将该参数替换掉
-print('高度和宽度数据字符串是：', width_height_str)
-img_url_final = srt.replace(width_height_str, '')  #把高度和宽度的字符串替换成空字符
-print('截取后的图片的url是：', img_url_final)
