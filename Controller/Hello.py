764d904a755ce3db52d12265f66b3fed5249ee51
wangmm1994 <769669064@qq.com>
2020-07-07 10:28:31 +0800

    Update Hello.py

--- Controller/Hello.py
+++ Controller/Hello.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def home():
     return render_template('home.html')
 
